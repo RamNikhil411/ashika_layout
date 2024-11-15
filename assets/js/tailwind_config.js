4315dc5eee22b9266db342382e467d58893adc94
RamNikhil411 <nikhil.b@orotron.com>
2024-11-15 15:18:49 +0530

    layout

---
+++ assets/js/tailwind_config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+      fontFamily: {
+        urbanist: ["Urbanist", "sans-serif"],
+      },
+      extend: {
+        colors: {
+          primary: "#9E9E9E",
+          secondary: "#08090A",
+          para: "#464646",
+          color: "#3B3632",
+        },
+      },
+    },
+  };
